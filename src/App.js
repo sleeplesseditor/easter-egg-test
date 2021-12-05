9496a7a1c975b6b48827e7a16ea22a6dabfb5c81
sleeplesseditor <josh@bryson.me.uk>
2021-12-05 23:09:50 +0000

    Extra text added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         {name.includes('Christmas') ? (
-          <Snowfall
-            // Changes the snowflake color
-            color="white"
-            // Applied to the canvas element
-            // style={{ background: '#fff' }}
-            // Controls the number of snowflakes that are created (default 150)
-            snowflakeCount={700}
-        />
+          <>
+            <Snowfall
+              // Changes the snowflake color
+              color="white"
+              // Applied to the canvas element
+              // style={{ background: '#fff' }}
+              // Controls the number of snowflakes that are created (default 150)
+              snowflakeCount={700}
+            />
+            <p>Here Be Snow?</p>
+          </>
         ) : null}
       </header>
     </div>
